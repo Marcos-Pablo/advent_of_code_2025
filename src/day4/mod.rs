d8d5a7ced8e4c130a5779edb1ea5bade858190cd
Marcos Pablo <marquin.pablo@gmail.com>
2026-05-16 16:42:34 -0300

    Day 4 part 2 solution

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn solve() {
-    let diagram = parse_diagram();
+    let mut diagram = parse_diagram();
     let adjacent_pos: [(isize, isize); 8] = [
         (-1, -1),
         (-1, 0),
@@ -14,46 +14,61 @@ pub fn solve() {
     ];
 
     let mut accessible_roll_papers = 0;
+    let mut pos_to_remove: Vec<(usize, usize)> = vec![];
 
-    for row in 0..diagram.len() {
-        for col in 0..diagram[row].len() {
-            if diagram[row][col] == '.' {
-                continue;
-            }
-
-            let mut count = 0;
-
-            for (x, y) in adjacent_pos {
-                let Some(new_row) = row.checked_add_signed(x) else {
-                    continue;
-                };
-
-                let Some(new_col) = col.checked_add_signed(y) else {
-                    continue;
-                };
-
-                if new_row >= diagram.len() {
+    loop {
+        for row in 0..diagram.len() {
+            for col in 0..diagram[row].len() {
+                if diagram[row][col] == '.' {
                     continue;
                 }
 
-                if new_col >= diagram[row].len() {
-                    continue;
+                let mut count = 0;
+
+                for (x, y) in adjacent_pos {
+                    let Some(new_row) = row.checked_add_signed(x) else {
+                        continue;
+                    };
+
+                    let Some(new_col) = col.checked_add_signed(y) else {
+                        continue;
+                    };
+
+                    if new_row >= diagram.len() {
+                        continue;
+                    }
+
+                    if new_col >= diagram[row].len() {
+                        continue;
+                    }
+
+                    if diagram[new_row][new_col] == '@' {
+                        count += 1;
+                    }
                 }
 
-                if diagram[new_row][new_col] == '@' {
-                    count += 1;
+                if count < 4 {
+                    accessible_roll_papers += 1;
+                    pos_to_remove.push((row, col));
                 }
-            }
-
-            if count < 4 {
-                accessible_roll_papers += 1;
             }
         }
+
+        if pos_to_remove.len() == 0 {
+            break;
+        }
+
+        remove_roll_papers(&pos_to_remove, &mut diagram);
+        pos_to_remove.clear();
     }
 
-    println!(
-        "there are {accessible_roll_papers} rolls of paper that can be accessed by a forklift"
-    );
+    println!("there are {accessible_roll_papers} rolls of paper that can be removed by a forklift");
+}
+
+fn remove_roll_papers(pos_to_remove: &[(usize, usize)], diagram: &mut [Vec<char>]) {
+    for &(row, col) in pos_to_remove {
+        diagram[row][col] = '.';
+    }
 }
 
 fn parse_diagram() -> Vec<Vec<char>> {
