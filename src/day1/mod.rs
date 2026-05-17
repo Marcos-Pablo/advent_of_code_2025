99d0adfc0c6522960eafbfdba237e1d940538660
Marcos Pablo <marquin.pablo@gmail.com>
2026-05-17 13:52:02 -0300

    chore(project structure): Moving core logic to lib.rs

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,35 +1,12 @@
-use std::io::BufRead;
-
-enum Rotation {
-    Left { num_rotations: i32 },
-    Right { num_rotations: i32 },
-}
-
-fn parse_rotations(reader: impl BufRead) -> impl Iterator<Item = Rotation> {
-    reader.lines().into_iter().map(|line| {
-        let line = line.expect("Error reading line");
-        let (direction, num_rotations) = line.trim().split_at(1);
-
-        let num_rotations: i32 = match num_rotations.trim().parse() {
-            Ok(num) if num >= 0 => num,
-            _ => panic!(
-                "Invalid number of rotations, number of rotations should a positive integer number"
-            ),
-        };
-
-        match direction {
-            "L" => Rotation::Left { num_rotations },
-            "R" => Rotation::Right { num_rotations },
-            _ => panic!(
-                "Invalid direction: {direction}, the allowed directions are L (Left) and R (Right)"
-            ),
-        }
-    })
-}
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 pub fn solve() {
     let file_path = "./src/day1/input.txt";
-    let reader = super::get_file_reader(file_path);
+    let file = File::open(file_path).expect("Error opening file");
+    let reader = BufReader::new(file);
     let rotations = parse_rotations(reader);
 
     let mut curr_pos: i32 = 50;
@@ -68,4 +45,31 @@ pub fn solve() {
     println!("The solution for part 1 is {password_part_1}");
     println!("The solution for part 2 is {password_part_2}");
     println!();
+}
+
+enum Rotation {
+    Left { num_rotations: i32 },
+    Right { num_rotations: i32 },
+}
+
+fn parse_rotations(reader: impl BufRead) -> impl Iterator<Item = Rotation> {
+    reader.lines().into_iter().map(|line| {
+        let line = line.expect("Error reading line");
+        let (direction, num_rotations) = line.trim().split_at(1);
+
+        let num_rotations: i32 = match num_rotations.trim().parse() {
+            Ok(num) if num >= 0 => num,
+            _ => panic!(
+                "Invalid number of rotations, number of rotations should a positive integer number"
+            ),
+        };
+
+        match direction {
+            "L" => Rotation::Left { num_rotations },
+            "R" => Rotation::Right { num_rotations },
+            _ => panic!(
+                "Invalid direction: {direction}, the allowed directions are L (Left) and R (Right)"
+            ),
+        }
+    })
 }
---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use std::io::{self, Write};
+
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+
+pub fn run() {
+    println!("Welcome to Advent Of Code 2025!");
+    let mut input = String::new();
+
+    loop {
+        println!("Type \"q\" to quit");
+        print!("Select the day you want to solve (1-25): ");
+        io::stdout().flush().unwrap();
+
+        input.clear();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading line");
+
+        if input.trim() == "q" {
+            println!("Bye!");
+            break;
+        }
+
+        let day: u8 = match input.trim().parse() {
+            Ok(num) if (1..=25).contains(&num) => num,
+            _ => {
+                println!("Type a valid number between 1 and 25 or \"q\" to quit");
+                continue;
+            }
+        };
+
+        println!("Running day {day} against the provided input\n");
+
+        match day {
+            1 => day1::solve(),
+            2 => day2::solve(),
+            3 => day3::solve(),
+            4 => day4::solve(),
+            5 => day5::solve(),
+            6 => day6::solve(),
+            _ => println!("day {day} solution not available yet"),
+        }
+    }
+}
